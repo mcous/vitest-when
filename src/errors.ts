044638c29075a0e174f4d6fcc16f0086544fd739
Michael Cousins <michael@cousins.io>
2023-12-04 20:22:52 -0500

    fixup: fix typo

--- src/errors.ts
+++ src/errors.ts
@@ -1,6 +1,6 @@
 export class NotAMockFunctionError extends TypeError {
   constructor(value: unknown) {
-    super(`when() must be given a Vite mock, but received ${String(value)}`)
+    super(`when() must be given a Vitest mock, but received ${String(value)}`)
     this.name = 'NotAMockFunctionError'
   }
 }

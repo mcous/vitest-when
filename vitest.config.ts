23c7e445bf2fe2c9a49d68fde9cf644a3acb9f8f
Michael Cousins <michael@cousins.io>
2025-10-26 23:14:48 -0400

    fixup: fix coverage more

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   test: {
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
       include: ['src/**/*'],
       reporter: ['text', 'html', 'lcovonly'],
     },

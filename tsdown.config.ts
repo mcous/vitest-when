e36365a162b76fe2cb85148d1b838d80b830b3ed
Michael Cousins <michael@cousins.io>
2026-09-07 00:07:08 -0400

    fixup: minor alignment with repo

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,16 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: 'src/vitest-when.ts',
-  format: 'esm',
-  fixedExtension: true,
-  sourcemap: true,
-  dts: true,
   failOnWarn: 'ci-only',
-  publint: {
-    level: 'error',
-  },
+  publint: true,
   attw: {
     profile: 'esm-only',
-    level: 'error',
   },
 })
